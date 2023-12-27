f82a4bb8154cbca37c1ade4cb8cb060dcf3bb833
epkite <epkite@ncsu.edu>
2023-12-27 13:31:34 -0500

    revert

--- csc342-2023Fall-GroupW-main/FinalProject/api/src/db/DBConnection.js
+++ csc342-2023Fall-GroupW-main/FinalProject/api/src/db/DBConnection.js
@@ -9,7 +9,8 @@ exports.getDatabaseConnection = () => {
       port: process.env.DB_PORT,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
+      database: process.env.MYSQL_DATABASE,
+      charset: process.env.DB_CHARSET
     })
   }
   return connection;
